7fa62ceb03c14fb0b71b33ee0a36fec294c1d0cf
FRANCISCO NAHUEL FONSECA <francisco60036@hotmail.com>
2025-11-06 18:40:10 -0300

    Creacion de tablas del campus, foros, hilos, comentarios, etc

---
+++ CreateDataBase.sql
@@ -0,0 +1,59 @@
+CREATE DATABASE BD2_TPI_G17
+GO
+
+USE BD2_TPI_G17 
+GO
+
+CREATE TABLE Cursos(
+IDCurso integer identity(50,1) not null PRIMARY KEY,
+CargaHoraria tinyint not null,
+Nombre varchar(100) not null
+);
+
+
+CREATE TABLE Comisiones(
+IDComision integer identity (1,1) PRIMARY KEY not null,
+NumeroComision integer not null,
+Turno varchar(10) not null,
+CantDias tinyint not null,
+HorarioComienzo tinyint not null,
+HorarioCierre tinyint not null,
+IDCurso integer not null --FK
+
+FOREIGN KEY (IDCurso) REFERENCES Cursos(IDCurso)
+);
+
+CREATE TABLE Foros(
+IDForo bigint identity(1,1) PRIMARY KEY not null,
+IDComision integer not null --FK
+
+FOREIGN KEY (IDComision) REFERENCES Comisiones(IDComision)
+);
+
+
+CREATE TABLE Hilos(
+IDHilo bigint identity(1,1) PRIMARY KEY not null,
+IDForo bigint not null --FK
+
+FOREIGN KEY (IDForo) REFERENCES Foros(IDForo)
+);
+
+CREATE TABLE Comentarios(
+IDComentario bigint identity(1,1) PRIMARY KEY,
+IDHilo bigint NOT NULL , --FK
+IDAutor bigint NOT NULL, --FK
+Comentario varchar(1000) not null,
+FechaComentario DATETIME not null,
+IDRespuesta bigint null
+
+FOREIGN KEY (IDHilo) REFERENCES HILOS(IDHilo)
+--FOREIGN KEY (IDAutor) REFERENCES --NO LA PUEDO HACER PORQUE TODAVIA NO ESTA LA TABLA USUARIOS
+);
+
+
+
+
+
+
+
+
